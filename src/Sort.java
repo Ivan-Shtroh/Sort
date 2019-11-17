9ab11511df3f60bbb5aefd5c0f4e4dbe1077d721
Ivan Shtroh <57430358+Ivan-Shtroh@users.noreply.github.com>
2019-11-17 21:27:51 +0700

    Update Sort.java

--- src/Sort.java
+++ src/Sort.java
@@ -2,24 +2,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sort {
-
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        String str2 = sc.nextLine();
-        String[] a = str2.split(" ");
-        int[] arr1 = new int[n];
+        int amountOfElem = Integer.parseInt(sc.nextLine());
+        String elements = sc.nextLine();
+        String[] elementsArrayStr = str2.split(" ");
+        int[] elementsArray = new int[n];
         int count = 0;
-        for (String i : a) {
-            arr1[count++] = Integer.parseInt(i);
+        for (String i : elementsArray) {
+            elementsArrayStr[count++] = Integer.parseInt(i);
         }
 
         int chetCount = 0;
         int nechCount = 0;
 
-        for (int value : arr1) { //посчитаем четные и нечетные числа
+        for (int value : elementsArray) { //посчитаем четные и нечетные числа
             if (value % 2 == 0) {
                 chetCount++;
             } else {
@@ -34,28 +32,28 @@ public class Sort {
             System.out.println(-1);
         } else if (chetCount == 0 || nechCount == 0) { //если нечетных или четных чисел нет
             System.out.println(0); //то перестановки не нужны
-            System.out.print(arr1[0]); // и этот элемент единственный (т.к. (chetCount - nechCount)<=1)
+            System.out.print(elementsArray[0]); // и этот элемент единственный (т.к. (chetCount - nechCount)<=1)
         } else { // если перестановки возможны
 
             //определим первый элемент
             if (chetCount > nechCount) { //если четных больше, начинаем с четного
-                if ((arr1[0] % 2) != 0) { //если первый элемент нечетный
-                    int temp = arr1[0];
+                if ((elementsArray[0] % 2) != 0) { //если первый элемент нечетный
+                    int temp = elementsArray[0];
                     for (int i = 1; i < n; i++) {
-                        if ((arr1[i] % 2) == 0) { //ставим на первое место (0-инд.) четный элемент
-                            arr1[0] = arr1[i];
-                            arr1[i] = temp;
+                        if ((elementsArray[i] % 2) == 0) { //ставим на первое место (0-инд.) четный элемент
+                            elementsArray[0] = arr1[i];
+                            elementsArray[i] = temp;
                             k++;
                         }
                     }
                 }
             } else if (chetCount < nechCount) { //если нечетных больше, начинаем с нечетного
-                if ((arr1[0] % 2) == 0) { //если первый элемент нечетный
+                if ((elementsArray[0] % 2) == 0) { //если первый элемент нечетный
                     int temp = arr1[0];
                     for (int i = 1; i < n; i++) {
-                        if ((arr1[i] % 2) != 0) {
-                            arr1[0] = arr1[i];
-                            arr1[i] = temp;
+                        if ((elementsArray[i] % 2) != 0) {
+                            elementsArray[0] = elementsArray[i];
+                            elementsArray[i] = temp;
                             k++;
                         }
                     }
@@ -65,13 +63,13 @@ public class Sort {
             //после того, как первый элемент определен, отсортируем оставшиеся элементы
 
             int i = 0;
-            for (int j = 1; j < arr1.length; j++) {
-                if ((arr1[i] % 2) == (arr1[j] % 2)) { //если рядом 2 четных или 2 нечетных элемента
-                    int temp = arr1[j];
-                    for (int m = j; m < arr1.length; m++) {
-                        if ((arr1[m] % 2) != (arr1[j] % 2)) {
-                            arr1[j] = arr1[m];
-                            arr1[m] = temp;
+            for (int j = 1; j < elementsArray.length; j++) {
+                if ((elementsArray[i] % 2) == (elementsArray[j] % 2)) { //если рядом 2 четных или 2 нечетных элемента
+                    int temp = elementsArray[j];
+                    for (int m = j; m < elementsArray.length; m++) {
+                        if ((elementsArray] % 2) != (elementsArray[j] % 2)) {
+                            elementsArray[j] = elementsArray[m];
+                            elementsArray[m] = temp;
                             k++;
                             break;
                         }
@@ -82,8 +80,8 @@ public class Sort {
 
             System.out.println(k);
 
-            String str1 = Arrays.toString(arr1).replaceAll("[\\[\\],]", " ").trim();
-            System.out.print(str1);
+            String result = Arrays.toString(elementsArray).replaceAll("[\\[\\],]", " ").trim();
+            System.out.print(result);
         }
 
     }
